052cda06d9662436cd11c219f16aa569c715a721
Alexey197 <margadonnn3@gmail.com>
2021-02-12 16:20:58 +0300

    model before classes

---
+++ src/model.js
@@ -0,0 +1,54 @@
+import image from './assets/image.png'
+
+const text = `
+  Крутые уроки по JavaScript
+`
+
+export const model = [
+  {type: 'title', value: 'Конструктор сайтов на чистом JavaScript', options: {
+      tag: 'h2',
+      // styles: `background: linear-gradient(to right, #ff0099, #493240); color: #fff;text-align: center;padding: 1.5rem`
+      styles: {
+        background: 'linear-gradient(to right, #ff0099, #493240)',
+        color: '#fff',
+        padding: '1.5rem',
+        'text-align': 'center'
+      }
+    }
+  },
+  {type: 'image', value: image, options: {
+      styles: {
+        padding: '2rem 0',
+        display: 'flex',
+        'justify-content': 'center'
+      },
+      imageStyles: {
+        width: '500px',
+        height: 'auto'
+      },
+      alt: 'Это картинка'
+    }
+  },
+  {
+    type: 'columns', value: [
+      'Приложение на чистом JavaScript, без использования библиотек',
+      'Узнаешь как работают принципы SOLID и ООП в JavaScript за один курс',
+      'JavaScript - это просто, интересно. Научись создавать любые UI своими руками'
+    ], options: {
+      styles: {
+        background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+        color: '#fff',
+        padding: '2rem',
+        'font-weight': 'bold'
+      }
+    }
+  },
+  {type: 'text', value: text, options: {
+    styles: {
+      background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+      padding: '1rem',
+      'font-weight': 'bold'
+    }
+    }
+  }
+]
